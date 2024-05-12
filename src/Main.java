31fb1afc1f414f538d2ad6023b2bba1fae662784
Bhavay Arora <bhavay.arora8@gmail.com>
2024-05-12 22:39:12 +0530

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void Try(String b)
+    {
+        System.out.println(b);
+    }
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it
+        int a[]= {1,2,4,5,8};
+        Try("whyyy");
+        for(int x:a)
+        {
+            System.out.println(x);
+        }
+        System.out.println("Hello and welcome!");
+        System.out.println("Lets Crack MNC");
+
+
+    }
+}
